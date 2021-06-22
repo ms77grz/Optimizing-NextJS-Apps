32ef1a213d64ac47b294313e29336e9776148889
ms77grz <ms77grz@gmail.com>
2021-06-22 17:57:09 +0300

    Extracting Data on the Catch-All Page - 65%

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,23 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import EventList from '../../components/events/event-list';
+import { getAllEvents } from '../../dummy-data';
+import EventsSearch from '../../components/events/events-search';
+
 function AllEventsPage() {
+  const events = getAllEvents();
+
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    router.push(`/events/${year}/${month}`);
+  }
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
 
